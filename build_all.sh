4aa74b22d365fbe1081c01973500acf6449357da
toledoJavier <toledojavier90@gmail.com>
2015-09-11 23:27:47 -0300

    Iniciando Kata chopper

--- build_all.sh
+++ build_all.sh
@@ -3,3 +3,7 @@ set -e
 cd ejemplo_banco
 bundle install
 bundle exec rake
+cd ..
+cd chopper
+bundle install
+bundle exec rake
---
+++ chopper/model/chopper.rb
@@ -0,0 +1,5 @@
+class Chopper
+  def chop(number, array)
+    -1
+  end
+end
