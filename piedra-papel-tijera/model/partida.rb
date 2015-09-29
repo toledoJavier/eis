e4c6f73b3a2249813193a412c727b6730ac309c0
toledoJavier <toledojavier90@gmail.com>
2015-09-29 20:53:43 -0300

    Refactor en metodo ganador? de Partida

--- piedra-papel-tijera/model/partida.rb
+++ piedra-papel-tijera/model/partida.rb
@@ -4,21 +4,39 @@ class Partida
     @jugador_dos= otro_jugador
     @rondas= []
   end
-
+  
+  #Agrega una ronda a la partida.
   def agregar_ronda(ronda)
     @rondas.push ronda
   end
-
+  
+  #Retorna el jugador que gano mas rondas, en caso de empate devuelve ambos
+  #jugadores en un array.
   def ganador?
     resultados= @rondas.map {|r| r.ganador?()}
     ganador_cant = resultados.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
-    posible_ganador= resultados.max_by { |v| ganador_cant[v] }
-    if ganador_cant.all? {|x| ganador_cant[x] == 1} || resultados.all? {|x| x.kind_of?(Array)}
+    if hay_empate? resultados, ganador_cant
     	[@jugador_uno, @jugador_dos]
-      elsif posible_ganador.kind_of? Array
-    	resultados.min_by { |v| ganador_cant[v] }
-      else
-      	  posible_ganador
+    else
+    	definir_ganador(resultados, ganador_cant)
+    end
+  end
+  
+  #Recibe un array con cada resultado de cada ronda, un hash con
+  #ganador=>cantidad de veces que gano y retorna true si hay empate
+  def hay_empate?(resultados, ganador_cant)
+    ganador_cant.all? {|x| ganador_cant[x] == 1} || resultados.all? {|x| x.kind_of?(Array)}
+  end
+  
+  #Recibe un array con cada resultado de cada ronda, un hash con
+  #ganador=>cantidad de veces que gano y el que ganador.
+  #precondicion: antes usar el metodo hay_empate?
+  def definir_ganador(resultados, ganador_cant)
+  	posible_ganador= resultados.max_by { |v| ganador_cant[v] }
+    if posible_ganador.kind_of? Array
+      resultados.min_by { |v| ganador_cant[v] }
+    else
+      posible_ganador
     end
   end
 end
