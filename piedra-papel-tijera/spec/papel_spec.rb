4a79be4938100ffba3df7860a9fa19aa297a1a43
toledoJavier <toledojavier90@gmail.com>
2015-09-29 22:51:26 -0300

    Testeando que papel pierda con mono

--- piedra-papel-tijera/spec/papel_spec.rb
+++ piedra-papel-tijera/spec/papel_spec.rb
@@ -5,7 +5,8 @@ require_relative '../model/tipo_jugada'
 describe 'Papel' do
   let(:papel) { Papel.new }  
   let(:tijera) { double(Tijera) }
-  
+  let(:mono) { double(Mono) }
+
   it 'Metodo sos_papel? retorna true' do
     expect(papel.sos_papel?).to eq true
   end
@@ -25,5 +26,10 @@ describe 'Papel' do
   it 'Metodo gana_a? retorna false, porque papel pierde con tijera' do
     allow(tijera).to receive(:sos_piedra?) { false }
     expect(papel.gana_a?(tijera)).to eq false
+  end
+
+  it 'Metodo gana_a? retorna false, porque papel pierde con mono' do
+    allow(mono).to receive(:sos_piedra?) { false }
+    expect(papel.gana_a?(mono)).to eq false
   end
 end
