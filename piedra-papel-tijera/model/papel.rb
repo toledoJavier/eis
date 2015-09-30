bc47b1e8080acebdf6e626fbe1ea334474a368d7
toledoJavier <toledojavier90@gmail.com>
2015-09-29 21:42:29 -0300

    Testeando que piedra pierda con Papel

---
+++ piedra-papel-tijera/model/papel.rb
@@ -0,0 +1,2 @@
+class Papel
+end
--- piedra-papel-tijera/spec/piedra_spec.rb
+++ piedra-papel-tijera/spec/piedra_spec.rb
@@ -2,13 +2,22 @@ require 'rspec'
 
 require_relative '../model/piedra'
 require_relative '../model/tijera'
+require_relative '../model/papel'
 
 describe 'Piedra' do
   let(:piedra) { Piedra.new }  
   let(:tijera) { double(Tijera) }
-  
+  let(:papel) { double(Papel) }
+
   it 'Metodo gana_a? retorna true, porque piedra gana a tijera' do
   	allow(tijera).to receive(:es_tijera?) { true }
     expect(piedra.gana_a?(tijera)).to eq true
   end
+
+  it 'Metodo gana_a? retorna false, porque piedra pierde con papel' do
+    allow(papel).to receive(:es_tijera?) { false }
+    expect(piedra.gana_a?(papel)).to eq false
+  end
+  
+
 end
