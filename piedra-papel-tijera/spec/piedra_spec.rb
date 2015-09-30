5400836d94e373ad9d0eb7ab061cf09a0ffb0c05
toledoJavier <toledojavier90@gmail.com>
2015-09-29 21:46:17 -0300

    Testeando que piedra vs mono de false, aunque sea empate

--- piedra-papel-tijera/spec/piedra_spec.rb
+++ piedra-papel-tijera/spec/piedra_spec.rb
@@ -3,11 +3,13 @@ require 'rspec'
 require_relative '../model/piedra'
 require_relative '../model/tijera'
 require_relative '../model/papel'
+require_relative '../model/mono'
 
 describe 'Piedra' do
   let(:piedra) { Piedra.new }  
   let(:tijera) { double(Tijera) }
   let(:papel) { double(Papel) }
+  let(:mono) { double(Mono) }
 
   it 'Metodo gana_a? retorna true, porque piedra gana a tijera' do
   	allow(tijera).to receive(:es_tijera?) { true }
@@ -19,5 +21,8 @@ describe 'Piedra' do
     expect(piedra.gana_a?(papel)).to eq false
   end
   
-
+  it 'Metodo gana_a? retorna false, porque piedra empata con mono' do
+    allow(mono).to receive(:es_tijera?) { false }
+    expect(piedra.gana_a?(mono)).to eq false
+  end
 end
